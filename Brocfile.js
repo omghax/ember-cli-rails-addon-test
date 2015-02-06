55473f6b3c9edcac7e556c7c696306c07ccb34ea
Dray Lacy <dray@envylabs.com>
2015-02-06 12:14:39 -0500

    Set storeConfigInMeta to true.

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,9 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  storeConfigInMeta: true
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
